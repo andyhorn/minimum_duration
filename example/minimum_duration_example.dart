69d16863e8e715a82a3de3fd65a5b5d40d24f002
Andy Horn <andyjhorn@gmail.com>
2023-10-12 13:06:21 -0700

    Initial Commit

---
+++ example/minimum_duration_example.dart
@@ -0,0 +1,6 @@
+import 'package:minimum_duration/minimum_duration.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/minimum_duration_test.dart
@@ -0,0 +1,16 @@
+import 'package:minimum_duration/minimum_duration.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
